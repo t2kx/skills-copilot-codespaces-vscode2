b0a595bc5bd4d3ed5706672ba2b3f004ddde4bfd
Mike Martin <37438394+t2kx@users.noreply.github.com>
2024-02-21 07:26:29 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,35 @@
+// creqate web server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// create a route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+```
+
+## 3. Create a route for comments
+
+```javascript
+// Path: comments.js
+// creqate web server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// create a route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/comments", (req, res) => {
+  res.json({
+    comments: [
+      {
+        id: 1,
